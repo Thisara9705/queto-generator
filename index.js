a586651e17cbe017f0bdfe34e75041eed73ca414
Wickramasinghe W D T D <102945450+Thisara9705@users.noreply.github.com>
2025-06-16 14:10:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import { generateTextAndImage } from "./utils.js"
+
+let name = "Thisara Wickramasinghe"
+let favoriteActivity = "meditation"
+let favoritePlace = "sri lanka"
+let temperature = 0.9
+
+generateTextAndImage(name, favoriteActivity, favoritePlace, temperature)
